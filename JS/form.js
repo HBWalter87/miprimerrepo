66537404670b9d7b5f2e7a6a6e11aaa41242641d
Martín Miselli <martin.miselli@gmail.com>
2023-11-14 11:45:21 -0300

    fix paths, validación del form, validación inputs ind

--- JS/form.js
+++ JS/form.js
@@ -3,17 +3,33 @@ document.addEventListener("DOMContentLoaded", function () {
   var apellido = document.getElementById("apellido");
   var email = document.getElementById("email");
 
-  nombre.addEventListener("input", verificarContenido);
-  apellido.addEventListener("input", verificarContenido);
-  email.addEventListener("input", verificarContenido);
+  nombre.addEventListener("blur", verificarNombre);
+  apellido.addEventListener("blur", verificarApellido);
+  email.addEventListener("blur", verificarEmail);
 
-  function verificarContenido() {
-    if (nombre.value.trim() !== '' && apellido.value.trim() !== '' && email.value.trim() !== '') {
-      btnEnviar.removeAttribute("disabled");
+  function verificarNombre() {
+    if (nombre.value.trim() !== '') {
+      nombre.style.backgroundColor = 'white'
     } else {
-      btnEnviar.setAttribute("disabled", "disabled");
+      nombre.style.backgroundColor = '#f15e5e7d'
     }
   }
+  function verificarApellido() {
+    if (apellido.value.trim() !== '') {
+      apellido.style.backgroundColor = 'white'
+    } else {
+      apellido.style.backgroundColor = '#f15e5e7d'
+    }
+  }
+  function verificarEmail() {
+    if (email.value.trim() !== '') {
+      email.style.backgroundColor = 'white'
+    } else {
+      email.style.backgroundColor = '#f15e5e7d'
+    }
+  }
+    
+  
 
 
   function validarCampo() {
@@ -21,12 +37,38 @@ document.addEventListener("DOMContentLoaded", function () {
   var apellido = document.getElementById("apellido").value;
   var email = document.getElementById("email").value;
 
-  if (nombre.trim() === '' || apellido.trim() === '' || email.trim() === '') {
-    alert("Por favor, complete todos los campos obligatorios.");
+  if (nombre.trim() === '' && apellido.trim() === '' && email.trim() === '') {
+    alert("Por favor, complete el nombre, el apellido y el email.");
+    return false;
+  }
+  else if (nombre.trim() === '' && apellido.trim()=== '') {
+    alert("Por favor, complete el nombre y el apellido.")
+    return false;
+  }
+  else if (nombre.trim() === '' && email.trim() === '') {
+    alert("Por favor, complete el nombre y el email.")
+    return false;
+  }
+  else if (apellido.trim() === '' && email.trim() === '') {
+    alert("Por favor, complete el apellido y el email.")
+    return false;
+  }
+  else if (nombre.trim() === '') {
+    alert("Por favor, complete su nombre.")
+    return false;
+  }
+  else if (apellido.trim() === '') {
+    alert("Por favor, complete su apellido.")
+    return false;
+  }
+  else if (email.trim() === '') {
+    alert("Por favor, complete su email.")
     return false;
   }
 
   return true; 
 }
+
+  window.validarCampo = validarCampo;
 
 });
